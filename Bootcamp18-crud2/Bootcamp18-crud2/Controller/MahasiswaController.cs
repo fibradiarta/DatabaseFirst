c899fcbf798836daa02b4d6dc89615f01824707d
DESKTOP-5ESCT6E\fibradiarta <fibradiarta@gmail.com>
2018-08-28 10:21:35 +0700

    fix bug minor : - Convert Datetime to Date format like dd/MM/yyyy - When we update data but we can find the ID it's will be show the error message

--- Bootcamp18-crud2/Bootcamp18-crud2/Controller/MahasiswaController.cs
+++ Bootcamp18-crud2/Bootcamp18-crud2/Controller/MahasiswaController.cs
@@ -118,69 +118,79 @@ namespace Bootcamp18_crud2.Controller
 
         public int Update(int id)
         {
-            string nama, alamat, telp, jenis_kelamin, universitas;
+            string nama, alamat, telp, jenis_kelamin, universitas, tanggal, temp_tanggal;
             DateTime tanggal_lahir;
             int jurusan_id;
 
             var temp_mahasiswa = context.tbl_mahasiswa.Find(id);
 
-            
-            Console.WriteLine("=========Data sebelum terupdate=========");
-            Console.WriteLine("Nama             : " + temp_mahasiswa.nama);
-            Console.WriteLine("Alamat           : " + temp_mahasiswa.alamat);
-            Console.WriteLine("Tanggal Lahir    : " + temp_mahasiswa.tanggal_lahir);
-            Console.WriteLine("No Telp          : " + temp_mahasiswa.no_telp);
-            Console.WriteLine("Jenis Kelamin    : " + temp_mahasiswa.jenis_kelamin);
-            Console.WriteLine("Nama Universitas : " + temp_mahasiswa.nama_universitas);
-            Console.WriteLine("Nama Jurusan     : " + temp_mahasiswa.tbl_jurusan.nama_jurusan);
-            Console.WriteLine("=========================================\n");
-            //Console.ReadKey(true);
-
-            Console.Write("Masukkan Nama Lengkap     : ");
-            nama = Console.ReadLine();
-            Console.Write("Masukkan Alamat           : ");
-            alamat = Console.ReadLine();
-            Console.Write("Masukkan Jenis Kelamin    : ");
-            jenis_kelamin = Console.ReadLine();
-            Console.Write("Masukkan No Telp          : ");
-            telp = Console.ReadLine();
-            Console.Write("Masukkan Tanggal lahir    : ");
-            tanggal_lahir = Convert.ToDateTime(Console.ReadLine());
-            Console.Write("Masukkan Nama Universitas : ");
-            universitas = Console.ReadLine();
-            Console.Write("Masukkan ID Jurusan       : ");
-            jurusan_id = Convert.ToInt32(Console.ReadLine());
-            var getJurusan = context.tbl_jurusan.Find(jurusan_id);
-            if (getJurusan == null)
+            if (temp_mahasiswa == null)
             {
-                Console.Write("ID Jurusan tidak ditemukan");
+                Console.Write("ID tidak ditemukan!");
+                Console.ReadKey(true);
             }
             else
             {
-                tbl_mahasiswa mahasiswa = SearchById(id);
-
-                mahasiswa.nama = nama;
-                mahasiswa.alamat = alamat;
-                mahasiswa.jenis_kelamin = jenis_kelamin;
-                mahasiswa.tanggal_lahir = tanggal_lahir;
-                mahasiswa.no_telp = telp;
-                mahasiswa.nama_universitas = universitas;
-                mahasiswa.id_jurusan = jurusan_id;
-                
-                context.Entry(mahasiswa).State = System.Data.Entity.EntityState.Modified;
-                context.SaveChanges();
-
-                Console.WriteLine("\n==========Data sesudah terupdate=========");
+                Console.WriteLine("=========Data sebelum terupdate=========");
                 Console.WriteLine("Nama             : " + temp_mahasiswa.nama);
                 Console.WriteLine("Alamat           : " + temp_mahasiswa.alamat);
-
-                Console.WriteLine("Tanggal Lahir    : " + temp_mahasiswa.tanggal_lahir);
+                tanggal = temp_mahasiswa.tanggal_lahir.ToString();
+                temp_tanggal = Convert.ToDateTime(tanggal).ToString("dd/MM/yyyy");
+                Console.WriteLine("Tanggal Lahir    : " + temp_tanggal);
                 Console.WriteLine("No Telp          : " + temp_mahasiswa.no_telp);
                 Console.WriteLine("Jenis Kelamin    : " + temp_mahasiswa.jenis_kelamin);
                 Console.WriteLine("Nama Universitas : " + temp_mahasiswa.nama_universitas);
                 Console.WriteLine("Nama Jurusan     : " + temp_mahasiswa.tbl_jurusan.nama_jurusan);
-                Console.WriteLine("=========================================");
-                Console.ReadKey(true);
+                Console.WriteLine("=========================================\n");
+                //Console.ReadKey(true);
+
+                Console.Write("Masukkan Nama Lengkap     : ");
+                nama = Console.ReadLine();
+                Console.Write("Masukkan Alamat           : ");
+                alamat = Console.ReadLine();
+                Console.Write("Masukkan Jenis Kelamin    : ");
+                jenis_kelamin = Console.ReadLine();
+                Console.Write("Masukkan No Telp          : ");
+                telp = Console.ReadLine();
+                Console.Write("Masukkan Tanggal lahir    : ");
+                tanggal_lahir = Convert.ToDateTime(Console.ReadLine());
+                Console.Write("Masukkan Nama Universitas : ");
+                universitas = Console.ReadLine();
+                Console.Write("Masukkan ID Jurusan       : ");
+                jurusan_id = Convert.ToInt32(Console.ReadLine());
+                var getJurusan = context.tbl_jurusan.Find(jurusan_id);
+                if (getJurusan == null)
+                {
+                    Console.Write("ID Jurusan tidak ditemukan");
+                }
+                else
+                {
+                    tbl_mahasiswa mahasiswa = SearchById(id);
+
+                    mahasiswa.nama = nama;
+                    mahasiswa.alamat = alamat;
+                    mahasiswa.jenis_kelamin = jenis_kelamin;
+                    mahasiswa.tanggal_lahir = tanggal_lahir;
+                    mahasiswa.no_telp = telp;
+                    mahasiswa.nama_universitas = universitas;
+                    mahasiswa.id_jurusan = jurusan_id;
+
+                    context.Entry(mahasiswa).State = System.Data.Entity.EntityState.Modified;
+                    context.SaveChanges();
+
+                    Console.WriteLine("\n==========Data sesudah terupdate=========");
+                    Console.WriteLine("Nama             : " + temp_mahasiswa.nama);
+                    Console.WriteLine("Alamat           : " + temp_mahasiswa.alamat);
+                    tanggal = mahasiswa.tanggal_lahir.ToString();
+                    temp_tanggal = Convert.ToDateTime(tanggal).ToString("dd/MM/yyyy");
+                    Console.WriteLine("Tanggal Lahir    : " + temp_tanggal);
+                    Console.WriteLine("No Telp          : " + temp_mahasiswa.no_telp);
+                    Console.WriteLine("Jenis Kelamin    : " + temp_mahasiswa.jenis_kelamin);
+                    Console.WriteLine("Nama Universitas : " + temp_mahasiswa.nama_universitas);
+                    Console.WriteLine("Nama Jurusan     : " + temp_mahasiswa.tbl_jurusan.nama_jurusan);
+                    Console.WriteLine("=========================================");
+                    Console.ReadKey(true);
+                }
             }
             return id;
         }
@@ -207,8 +217,9 @@ namespace Bootcamp18_crud2.Controller
                     Console.WriteLine("Id Mahasiswa     : " + mahasiswa.id);
                     Console.WriteLine("Nama             : " + mahasiswa.nama);
                     Console.WriteLine("Alamat           : " + mahasiswa.alamat);
-                    
-                    Console.WriteLine("Tanggal Lahir    : " + mahasiswa.tanggal_lahir);
+                    string tanggal = mahasiswa.tanggal_lahir.ToString();
+                    string temp_tanggal = Convert.ToDateTime(tanggal).ToString("dd/MM/yyyy");
+                    Console.WriteLine("Tanggal Lahir    : " + temp_tanggal);
                     Console.WriteLine("No Telp          : " + mahasiswa.no_telp);
                     Console.WriteLine("Jenis Kelamin    : " + mahasiswa.jenis_kelamin);
                     Console.WriteLine("Nama Universitas : " + mahasiswa.nama_universitas);
@@ -245,6 +256,8 @@ namespace Bootcamp18_crud2.Controller
                 Console.WriteLine("=========================================");
                 Console.WriteLine("Nama             : " + mahasiswa.nama);
                 Console.WriteLine("Alamat           : " + mahasiswa.alamat);
+                string tanggal = mahasiswa.tanggal_lahir.ToString();
+                string temp_tanggal = Convert.ToDateTime(tanggal).ToString("dd/MM/yyyy");
                 Console.WriteLine("Tanggal Lahir    : " + mahasiswa.tanggal_lahir);
                 Console.WriteLine("No Telp          : " + mahasiswa.no_telp);
                 Console.WriteLine("Jenis Kelamin    : " + mahasiswa.jenis_kelamin);
